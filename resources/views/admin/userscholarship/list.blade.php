7f7fa962b607124e683a9b91fd94cc43562a690c
suhailharitsah <msuhailh354@gmail.com>
2023-11-23 00:10:19 +0700

    make pengusul new

--- resources/views/admin/userscholarship/list.blade.php
+++ resources/views/admin/userscholarship/list.blade.php
@@ -3,35 +3,41 @@
 @section('navbar', 'Pengusul')
 
 @section('content')
-    <h2 class="text-lg font-semibold mb-4">List Pendaftar Beasiswa</h2>
-    <div class="table-responsive">
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+    <h2 class="text-lg font-semibold mb-4">Daftar Nama Beasiswa</h2>
 
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
-        <table class="min-w-full bg-white border border-gray-300 text-sm">
+
+    <div class="mb-3 flex items-center relative">
+        <div class="flex items-center">
+            <input type="text" id="search" placeholder="Search..."
+                class="p-1 border-2 rounded-lg border-gray-300 focus:ring-1 focus:ring-sky-500 placeholder:text-slate-300 placeholder:text-sm">
+        </div>
+    </div>
+
+    <div class="table-responsive">
+        <table class=" bg-white border border-gray-300 text-base border-collapse w-full">
             <thead>
                 <tr class="border-b-2 bg-sky-800 text-white text-start">
-                    <th class="py-2 px-2 border-r">No</th>
-                    <th class="py-2 px-2 border-r">Nama Beasiswa</th>
+                    <th class="p-2 w-[5%] text-center border border-gray-300">No</th>
+                    <th class="p-2 border table-fixed border-gray-300">Nama Beasiswa</th>
+                    <th class="p-2 border w-1/12 text-center border-gray-300">Tahun</th>
                 </tr>
             </thead>
             <tbody>
+                <?php $i = 1; ?>
                 @foreach ($scholarships as $item)
-                    <tr class="text-start">
-                        <td class="py-2 px-2 border-r">{{ $loop->index + 1 }}</td>
-                        <td class="py-2 px-2 border-r"><a href="{{ route('registrations.index', ['scholarship_id' => $item->id ]) }}">
-                        {{ $item->name }}</a></td>
+                    <tr class="table-fixed border border-gray-300 @if ($loop->even) @else bg-slate-50 @endif">
+                        <td class="p-2 w-[5%] text-center border border-gray-300">{{ $loop->index + 1 }}</td>
+                        <td class="p-2 border table-fixed border-gray-300"><a
+                                href="{{ route('registrations.index', ['scholarship_id' => $item->id]) }}">{{ $item->name }}</a>
+                        </td>
+                        <td class="p-2 border w-1/12 text-center border-gray-300">{{ $item->year }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+
+
+
+
 @endsection
