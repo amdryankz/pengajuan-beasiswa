5190655112fef8758b4eb64968b6b7e4416eb914
suhailharitsah <msuhailh354@gmail.com>
2024-01-15 13:02:25 +0700

    tabel beranda scrollnya ada dua

--- resources/views/admin/alumni/index.blade.php
+++ resources/views/admin/alumni/index.blade.php
@@ -4,17 +4,20 @@
 
 @section('content')
     <div class="mb-8 text-start text-sm bg-white p-4">
-        <a href="{{ route('alumni.list') }}" class="inline-flex items-start px-2 py-1 mb-4 text-blue-600 hover:bg-blue-100 rounded-lg">
-            <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <a href="{{ route('alumni.list') }}"
+            class="inline-flex items-start px-2 py-1 mb-4 text-blue-600 hover:bg-blue-100 rounded-lg">
+            <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
             </svg>
         </a>
 
         <div>
-            <h2 class="text-lg font-semibold mb-4">List Beasiswa Alumni - {{ $scholarship->name }}</h2>
+            <h2 class="text-lg font-semibold mb-4">List Beasiswa Alumni - {{ $scholarship->scholarship->name }}</h2>
 
             <div class="mb-10">
-                <a href="{{ route('alumni.downloadExcel', ['scholarship_id' => $scholarship->id]) }}" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+                <a href="{{ route('alumni.downloadExcel', ['scholarship_id' => $scholarship->id]) }}"
+                    class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
                     Export to Excel <ion-icon name="logo-buffer"></ion-icon>
                 </a>
             </div>
@@ -22,15 +25,18 @@
             <div class="flex items-center mb-2 md:w-1/2">
                 <div class="w-3/4 pr-2">
                     <label for="facultyFilter" class="block text-sm font-medium text-gray-700">Pilih Fakultas:</label>
-                    <select id="facultyFilter" name="facultyFilter" class="block w-full p-2 border-1 border-solid border-neutral-200 rounded-md focus:border-sky-500">
+                    <select id="facultyFilter" name="facultyFilter"
+                        class="block w-full p-2 border-1 border-solid border-neutral-200 rounded-md focus:border-sky-500">
                         <option value="">Semua Fakultas</option>
                         @foreach ($data as $item)
                             @if (is_array($item['fakultasList']))
                                 @foreach ($item['fakultasList'] as $fakultas)
-                                    <option value="{{ htmlspecialchars($fakultas) }}">{{ htmlspecialchars($fakultas) }}</option>
+                                    <option value="{{ htmlspecialchars($fakultas) }}">{{ htmlspecialchars($fakultas) }}
+                                    </option>
                                 @endforeach
                             @else
-                                <option value="{{ htmlspecialchars($item['fakultasList']) }}">{{ htmlspecialchars($item['fakultasList']) }}</option>
+                                <option value="{{ htmlspecialchars($item['fakultasList']) }}">
+                                    {{ htmlspecialchars($item['fakultasList']) }}</option>
                             @endif
                         @endforeach
                     </select>
@@ -64,12 +70,14 @@
                             <td class="py-2 px-2 border-r">{{ $item['user']->fakultas }}</td>
                             <td class="py-2 px-2 border-r">{{ $item['user']->prodi }}</td>
                             <td class="py-1 px-1 border-r text-center">
-                                <a href="{{ route('alumni.detail', ['user_id' => $item['user']->id, 'scholarship_id' => $item['scholarship']->id]) }}" class="bg-blue-500 text-white py-1 px-2 rounded hover:bg-blue-700 font-normal">
+                                <a href="{{ route('alumni.detail', ['user_id' => $item['user']->id, 'scholarship_id' => $item['scholarship']->id]) }}"
+                                    class="bg-blue-500 text-white py-1 px-2 rounded hover:bg-blue-700 font-normal">
                                     Detail
                                 </a>
                             </td>
                             <td class="py-1 px-1 text-center">
-                                <a href="{{ route('admin.scholarship.pdf', ['user_id' => $item['user']->id, 'scholarship_id' => $item['scholarship']->id]) }}" target="_blank">
+                                <a href="{{ route('admin.scholarship.pdf', ['user_id' => $item['user']->id, 'scholarship_id' => $item['scholarship']->id]) }}"
+                                    target="_blank">
                                     <span class="flex items-center justify-center">
                                         <i class="bi bi-file-pdf-fill mr-1"></i>
                                         <span>Cetak PDF</span>
--- resources/views/admin/homepage/index.blade.php
+++ resources/views/admin/homepage/index.blade.php
@@ -8,7 +8,7 @@
 
             <!-- Card 1 -->
             <div
-                class="bg-gradient-to-r from-purple-500 via-blue-500 to-cyan-500 p-8 m-6 rounded-3xl w-80 flex flex-col items-center">
+                class="bg-gradient-to-r from-purple-500 via-blue-500 to-cyan-500 p-8 m-6 rounded-3xl w-64 flex flex-col items-center">
                 <i class="bi bi-mortarboard-fill text-white text-4xl mb-4"></i>
                 <h2 class="text-white text-xl mb-2 text-center">Mahasiswa Aktif Beasiswa USK Keseluruhan</h2>
                 <p class="text-white text-lg text-center">
@@ -18,7 +18,7 @@
 
             <!-- Card 2 -->
             <div
-                class="bg-gradient-to-r from-teal-500 via-green-500 to-lime-500 p-8 m-6 rounded-3xl w-80 flex flex-col items-center">
+                class="bg-gradient-to-r from-teal-500 via-green-500 to-lime-500 p-8 m-6 rounded-3xl w-64 flex flex-col items-center">
                 <i class="bi bi-wallet-fill text-white text-4xl mb-4"></i>
                 <h2 class="text-white text-xl mb-2 text-center">Jumlah Beasiswa Keseluruhan</h2>
                 <p class="text-white text-lg text-center">
@@ -28,7 +28,7 @@
 
             <!-- Card 3 -->
             <div
-                class="bg-gradient-to-r from-amber-500 via-orange-500 to-red-500 p-8 m-6 rounded-3xl w-80 flex flex-col items-center">
+                class="bg-gradient-to-r from-amber-500 via-orange-500 to-red-500 p-8 m-6 rounded-3xl w-64 flex flex-col items-center">
                 <i class="bi bi-bar-chart-fill text-white text-4xl mb-4"></i>
                 <h2 class="text-white text-xl mb-2 text-center"> Total Alumni Beasiswa USK</h2>
                 <p class="text-white text-lg text-center">
@@ -37,48 +37,72 @@
             </div>
 
             <!-- Table -->
-            <div class="w-full m-6">
-                <h2 class="text-xl ml-6 mb-4">Jumlah Mahasiswa per Beasiswa</h2>
-                <div class="overflow-x-auto">
-                    <table id="{{-- myTable --}}" class="min-w-full bg-white border border-gray-300 text-sm">
-                        <thead>
-                            <tr class="bg-blue-500 text-white">
-                                <th class="py-2 px-4 border-b">Nama Beasiswa</th>
-                                <th class="py-2 px-4 border-b text-center">Tahun Beasiswa</th>
-                                <th class="py-2 px-4 border-b">Fakultas</th>
-                                <th class="py-2 px-4 border-b">Jumlah</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($scholarshipData)
-                                @foreach ($scholarshipData as $scholarshipId => $data)
-                                    @foreach ($fakultasList as $fakultas)
-                                        <tr>
-                                            <td class="py-2 px-4 border-b">{{ $data['name'] }}</td>
-                                            <td class="py-2 px-4 border-b text-center">{{ $data['year'] }}</td>
-                                            <td class="py-2 px-4 border-b">{{ $fakultas }}</td>
-                                            <td class="py-2 px-4 border-b">{{ $data['facultyTotals']->get($fakultas, 0) }}
-                                            </td>
-                                        </tr>
-                                    @endforeach
+            <div class="w-full m-6 overflow-x-auto">
+                <h2 class="text-xl mb-4">Jumlah Mahasiswa per Beasiswa</h2>
+
+                <!-- Pencarian -->
+                <div class="mb-4">
+                    <label for="search" class="block text-sm font-medium text-gray-700">Cari Beasiswa:</label>
+                    <input type="text" id="search" name="search" class="mt-1 p-2 border rounded-md w-full"
+                        placeholder="Nama Beasiswa">
+                </div>
+
+                <table id="{{-- myTable --}}" class="w-full bg-white border border-gray-300 text-sm">
+                    <thead>
+                        <tr class="bg-blue-500 text-white">
+                            <th class="px-4 py-2 border-b">Nama Beasiswa</th>
+                            <th class="px-4 py-2 border-b">Tahun Beasiswa</th>
+                            <th class="px-4 py-2 border-b">Fakultas</th>
+                            <th class="px-4 py-2 border-b">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($scholarshipData)
+                            @foreach ($scholarshipData as $scholarshipId => $data)
+                                @foreach ($fakultasList as $fakultas)
                                     <tr>
-                                        <td colspan="2" class="py-2 px-4 border-b"></td>
-                                        <td class="py-2 px-4 border-b font-semibold">Total</td>
-                                        <td class="py-2 px-4 border-b font-semibold">{{ $data['total'] }}</td>
+                                        <td class="px-4 py-2 border-b">{{ $data['name'] }}</td>
+                                        <td class="px-4 py-2 border-b">{{ $data['year'] }}</td>
+                                        <td class="px-4 py-2 border-b">{{ $fakultas }}</td>
+                                        <td class="px-4 py-2 border-b">{{ $data['facultyTotals']->get($fakultas, 0) }}</td>
                                     </tr>
                                 @endforeach
-                            @else
+
                                 <tr>
-                                    <td colspan="4" class="py-2 px-4 border-b text-center">Data Beasiswa tidak tersedia.
-                                    </td>
+                                    <td colspan="2" class="px-4 py-2 border-b font-bold">Total</td>
+                                    <td class="px-4 py-2 border-b"></td>
+                                    <td class="px-4 py-2 border-b font-bold">{{ $data['total'] }}</td>
                                 </tr>
-                            @endif
-                        </tbody>
-                    </table>
-                </div>
+                                <tr>
+                                    <td colspan="4" class="bg-blue-400 h-4"></td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="4" class="px-4 py-2 border-b text-center">Data Beasiswa tidak tersedia.</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
             </div>
-
-
         </div>
     </div>
+
+    <!-- Script Pencarian -->
+    <script>
+        const searchInput = document.getElementById('search');
+        const rows = document.querySelectorAll('tbody tr');
+
+        searchInput.addEventListener('input', function() {
+            const term = searchInput.value.toLowerCase();
+            rows.forEach(row => {
+                const name = row.querySelector('td:first-child').textContent.toLowerCase();
+                if (name.includes(term)) {
+                    row.style.display = 'table-row';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        });
+    </script>
 @endsection
